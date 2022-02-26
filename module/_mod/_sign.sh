3510d4e4e6c14fd20d1b7dccaa26a0a07b32ad02
Miseryset <1598654374@qq.com>
2022-02-26 16:34:07 +0800

    update

--- module/_mod/_sign.sh
+++ module/_mod/_sign.sh
@@ -5,28 +5,20 @@ upk_dir=${sh_dir}/_sign_source_upk
 
 mkdir -p ${upk_dir}
 tar -xf "${sh_dir}/_sign_source.tar.xz" -C "${upk_dir}" >/dev/null 2>&1
-toilet=${upk_dir}/toilet
 figlet=${upk_dir}/figlet
-set_perm ${toilet} 0 0 0777
 set_perm ${figlet} 0 0 0777
 
 print_code(){
 	ui_print ""
 	ui_print ""
-	rnd=$((${RANDOM}%8+1))
+	rnd=$((${RANDOM}%5+1))
 	if [ ${rnd} == 1 ];then
-		${toilet} -t "${1}"
+		${figlet} -d ${upk_dir} -f standard.flf "          ${1} "
 	elif [ ${rnd} == 2 ];then
-		${toilet} -t -d ${upk_dir} -f pagga.tlf " ${1} "
-	elif [ ${rnd} == 3 ];then
-		${toilet} -t -d ${upk_dir} -f letter.tlf " ${1} "
-	elif [ ${rnd} == 4 ];then
-		${figlet} "          ${1} "
-	elif [ ${rnd} == 5 ];then
 		${figlet} -d ${upk_dir} -f big.flf "          ${1} "
-	elif [ ${rnd} == 6 ];then
+	elif [ ${rnd} == 3 ];then
 		${figlet} -d ${upk_dir} -f lean.flf -w 200 " ${1} "
-	elif [ ${rnd} == 7 ];then
+	elif [ ${rnd} == 4 ];then
 		${figlet} -d ${upk_dir} -f block.flf -w 200 " ${1} "
 	else
 		${figlet} -d ${upk_dir} -f banner.flf " ${1} "
--- module/_mod/_thank.sh
+++ module/_mod/_thank.sh
@@ -2,9 +2,9 @@
 sh_dir=${TMPDIR}/_mod
 
 ui_print ""
-ui_print "---  Thanks："
+ui_print "---  致谢："
 for _name in $(cat ${sh_dir}/_thank_list.conf | grep -v '^#' ); do
 	ui_print "------  $_name"
 done
-ui_print "---  etc."
+ui_print "---  等..."
 ui_print ""
